651f44e9007db648d59b4578708c32f629d29442
Armada6199 <moh.abdin27@gmail.com>
2024-05-02 13:57:18 +0300

    fix authorization side drawer

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,21 +1,26 @@
 import { Grid } from "@mui/material";
 import { Helmet } from "react-helmet";
 import UserTable from "./UserTable";
+import { useContext } from "react";
+import { LoginContext } from "src/hooks/Context/LoginInfoContext";
+import { redirect } from "react-router";
 
 const User = () => {
   // media queries
+  const { loginData } = useContext(LoginContext);
+  if (loginData.role == "ADMIN" || loginData.role == "TEAM_LEAD")
+    return (
+      <>
+        <Helmet>
+          <title>Users | RoboAgent</title>
+        </Helmet>
 
-  return (
-    <>
-      <Helmet>
-        <title>Users | RoboAgent</title>
-      </Helmet>
-
-      <Grid container bgcolor={"red"}>
-        <UserTable />
-      </Grid>
-    </>
-  );
+        <Grid container bgcolor={"red"}>
+          <UserTable />
+        </Grid>
+      </>
+    );
+  else redirect("/dash/dashboard");
 };
 
 export default User;
